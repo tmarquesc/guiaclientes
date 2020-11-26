197f42bad66dc7ab17e78f774d03957739f004ea
Thiago Marques Cavalcante <plates@hotmail.com>
2020-11-25 22:38:22 -0300

    adicionando arquivos no repositorio

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div v-for="cliente in clientes" :key="cliente.id">
+      <Cliente :cliente="cliente"/>
+      <hr>
+      <h4>Edição: </h4>
+      <input type="text" v-model="cliente.nome">
+      <input type="text" v-model="cliente.email">
+    </div>
+  </div>
+</template>
+
+<script>
+import Cliente from './components/Cliente';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      clientes: [
+        {
+          id: 2,
+          nome: "Victor Lima",
+          email: "victor@lima.com",
+          idade: 99
+        },
+        {
+          id: 22,
+          nome: "Michael Scott",
+          email: "michael@lima.com",
+          idade: 45
+        },
+        {
+          id: 31,
+          nome: "Ednaldo Pereira",
+          email: "ednaldopereira@yahoo.com",
+          idade: 34
+        }
+      ]
+    }
+  },
+  components: {
+    Cliente
+  }
+}
+</script>
+
+<style>
+
+</style>
